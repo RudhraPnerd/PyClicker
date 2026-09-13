169b5cb996cbdf3103bb4b0e4ad93552819aa73f
Rudhra <rudhra.thiru@gmail.com>
2026-09-13 16:01:26 +1000

    v6

--- score_func.py
+++ score_func.py
@@ -1,23 +1,16 @@
-import pygame
+import os
+
 
 def read_score(file_path):
+    if not os.path.exists(file_path):
+        save_score(file_path, 0)
+        return 0
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            return int(file.read().strip())
-    except (FileNotFoundError, ValueError):
+        with open(file_path, 'r') as f:
+            return int(f.read().strip())
+    except (ValueError, FileNotFoundError):
         return 0
 
-def save_score(file_path, score_val):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(str(score_val))
-
-def get_rounded_image(image, corner_radius):
-    rect = image.get_rect()
-    mask_surface = pygame.Surface(rect.size, pygame.SRCALPHA)
-
-    pygame.draw.rect(mask_surface, (255, 255, 255, 255), rect, border_radius=corner_radius)
-
-    rounded_image = image.copy()
-    rounded_image.blit(mask_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
-
-    return rounded_image
+def save_score(file_path, score):
+    with open(file_path, 'w') as f:
+        f.write(str(score))
