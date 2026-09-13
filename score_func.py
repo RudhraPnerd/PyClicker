faddd35b92f4f2997d8d228b8de66accb5f220d6
Rudhra <rudhra.thiru@gmail.com>
2026-09-13 12:14:08 +1000

    v3

--- score_func.py
+++ score_func.py
@@ -1,3 +1,5 @@
+import pygame
+
 def read_score(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -8,3 +10,14 @@ def read_score(file_path):
 def save_score(file_path, score_val):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(str(score_val))
+
+def get_rounded_image(image, corner_radius):
+    rect = image.get_rect()
+    mask_surface = pygame.Surface(rect.size, pygame.SRCALPHA)
+
+    pygame.draw.rect(mask_surface, (255, 255, 255, 255), rect, border_radius=corner_radius)
+
+    rounded_image = image.copy()
+    rounded_image.blit(mask_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MIN)
+
+    return rounded_image
